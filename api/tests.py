17e6d3b20e3d51ecdaa59b97fd5bea3a9387523d
Andre Wong <andre@andre-wong.com>
2019-02-17 00:08:47 +1000

    Added api test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .tasks import fetch_url
+
+class TestAPI(TestCase):
+    def test_get_jobs(self):
+        response = self.client.get('/api/get_jobs')
+        self.assertEqual(response.json(), [])
